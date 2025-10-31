c61cdbc02a89b04c650ea36b99c4c552598037e4
zabavin81 <zabavin81@gmail.com>
2025-10-31 19:09:02 +0700

    feat(admin): add category creation with controller, route and page

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Category\StoreRequest;
 use App\Http\Resources\Category\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -18,5 +19,15 @@ class CategoryController extends Controller
     public function show(Category $category){
         $category = CategoryResource::make($category)->resolve();
         return inertia('Admin/Category/Show',compact('category'));
+    }
+
+    public function create(){
+        return inertia('Admin/Category/Create');
+    }
+
+    public function store(StoreRequest $request){
+        $data = $request->validated();
+        $category = Category::create($data);
+        return CategoryResource::make($category)->resolve();
     }
 }
---
+++ resources/js/Pages/Admin/Category/Create.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class = "mt-3 rounded-md bg-teal-600 mb-3 inline-block px-3 py-2 text-white hover:bg-teal-400">
+        <Link class = "" :href="route('admin.categories.index')">BACK</Link>
+    </div>
+    <div>
+        <div class="mb-4">
+            <input v-model ="category.title" class="border-gray-200 p-4 w-full" type="text" placeholder="title">
+        </div>
+        <div class="rounded-md bg-teal-600 mb-3 inline-block px-3 py-2 text-white hover:bg-teal-400">
+            <a href="#" @click.prevent="storeCategory()" class="">STORE</a>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import AdminLayout from '@/Layouts/AdminLayout.vue';
+import { Link } from '@inertiajs/vue3';
+
+export default {
+    name: 'Index',
+    layout: AdminLayout,
+
+    data() {
+        return{
+            category:{}
+        }
+    },
+    components: {
+        Link
+    },
+
+    methods: {
+        storeCategory(){
+            axios.post(route('admin.categories.store'),this.category)
+                .then((res) => {
+                    this.category = {};
+                })
+                .catch((e) => {
+
+                })
+                .finally(() => {
+
+                })
+        }
+    },
+    props: {
+        categories: {
+            type: Array,
+            required: false,
+        },
+    },
+};
+</script>
+
